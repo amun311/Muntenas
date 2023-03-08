3822a8a8858c5e59fda1e2eafd9fd28d825afb5b
amun311 <123638470+amun311@users.noreply.github.com>
2023-03-08 18:19:24 +0000

    formatul si aparenta

--- app.py
+++ app.py
@@ -2,33 +2,13 @@ from flask import Flask
 from flask import render_template 
 app = Flask(__name__)
 
-FAMILY = [
-  {
-    'id': 1,
-    'fam': 'Tata',
-    'nombre': 'Alexandru Gheorghe',
-    'edad': '39'
-  },
-  {
-    'id': 2,
-    'fam': 'Mama',
-    'nombre': 'Florentina Rodica',
-    'edad': '40'
-  },
-  {
-   'id': 3,
-    'fam': 'Fiu',
-    'nombre': 'Alexandru Theodor',
-    'edad': '17'
-  }
-]
 
 #@app.route('/')
 #def index():
 #    return 'Hello from Flask!'
 @app.route('/')
 def hello():
-  return render_template('home.html',family = FAMILY, family_name = 'Muntenas')
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
